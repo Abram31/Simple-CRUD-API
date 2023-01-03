c0b23aaab525e9559a003cb6bf9af0b1085e739b
Abram31 <a447623993@gmail.com>
2023-01-03 22:50:18 +0100

    feat: add example of server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+import { createServer } from 'http';
+
+const PORT = process.env.PORT || 4000;
+const server = createServer((req, res) => {
+  //   res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(
+    JSON.stringify({
+      data: 'Hello World!',
+    })
+  );
+});
+server.listen(PORT, () => {
+  console.log(`Worker ${process.pid} server running at http://localhost:${PORT}/`);
+});
+
 console.log('Hello Node!');
 console.log('Hello Node!!!!!!');
-const div = () => {
-  const alfa = 123;
-  const a = 5;
-};
